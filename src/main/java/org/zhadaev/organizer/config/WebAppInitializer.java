f3dd2dcc77c149c87715e5e4a144a80974532b2b
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-07-28 20:03:00 +0400

    added config classes

---
+++ src/main/java/org/zhadaev/organizer/config/WebAppInitializer.java
@@ -0,0 +1,27 @@
+package org.zhadaev.organizer.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context
+                = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);;
+
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic dispatcher = servletContext
+                .addServlet("dispatcher", new DispatcherServlet(context));
+
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
