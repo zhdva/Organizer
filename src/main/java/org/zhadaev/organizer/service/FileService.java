7c769159d5aceb14a0ca1075bd2f6cc09f0eef55
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-08-02 13:10:49 +0400

    rebuilded FileService

--- src/main/java/org/zhadaev/organizer/service/FileService.java
+++ src/main/java/org/zhadaev/organizer/service/FileService.java
@@ -1,59 +1,62 @@
 package org.zhadaev.organizer.service;
 
+import org.springframework.stereotype.Service;
+
 import java.io.*;
-import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
+@Service
 public class FileService {
 
-    private static String task;
-    private static String text;
-
-    //сохранение файла для задачи Expander
-    public static String save(final String taskName, final String number) {
-        task = taskName;
-        text = "task=" + task + "\n" + "number=" + number;
-        return save();
-    }
-
-    //сохранение файла для задачи Comparator
-    public static String save(final String taskName, final String a1, final String a2) {
-        task = taskName;
-        text = "task=" + task + "\n" +
-                "array1=" + a1 + "\n" +
-                "array2=" + a2;
-        return save();
-    }
-
     //сохранение файла
-    private static String save() {
-        FileWriter writer;
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH-mm-ss");
-        String date = sdf.format(new Date());
-        String filePath = getPath() + task + " " + date + ".txt";
+    public File save(final Map<String, String> map) {
+
+        File file;
         try {
-            writer = new FileWriter(filePath, false);
-            writer.write(text);
+            file = File.createTempFile(map.get("task") + " " + new Date().getTime(), ".txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        StringBuilder text = new StringBuilder();
+        switch (map.get("task")) {
+            case "Comparator":
+                text.append("task=").append(map.get("task")).append("\n");
+                text.append("array1=").append(map.get("array1")).append("\n");
+                text.append("array2=").append(map.get("array2"));
+                break;
+            case "Expander":
+                text.append("task=").append(map.get("task")).append("\n");
+                text.append("number=").append(map.get("number"));
+                break;
+            default:
+                return null;
+        }
+
+        FileWriter writer;
+        try {
+            writer = new FileWriter(file);
+            writer.write(text.toString());
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return filePath;
+
+        return file;
     }
 
     //открытие файла
-    public static Map<String, String> open(final File file) {
+    public Map<String, String> open(final File file) {
 
         Map<String, String> map = new HashMap<>(); //таблица для дальнейшего вывода результата
-        final String exception = "Exception";
 
         FileReader fr;
         try {
             fr = new FileReader(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            map.put(exception, "Файл не найден"); //запись ошибки в таблицу
+            map.put("message", "Файл не найден"); //запись ошибки в таблицу
             return map; //возврат таблицы с дальнейшим выводом ошибки
         }
 
@@ -81,25 +84,11 @@ public class FileService {
         }
 
         if (!map.containsKey("task") || !(map.containsKey("number") || map.containsKey("array1") && map.containsKey("array2"))) {
-            map.put(exception, "Неверная структура файла");
+            map.put("message", "Неверная структура файла");
         }
 
         return map;
 
-    }
-
-    //определение директории файла с программой
-    protected static String getPath() {
-        File file = null;
-        try {
-            file = new File(FileService.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        assert file != null;
-        String path = file.getPath();
-        String fileName = file.getName();
-        return path.substring(0, path.length() - fileName.length());
     }
 
 }
